9c1f30c0dae6b460672781463492d5ca034e58cc
Gaëtan Tokotuu <15010639+Veithne@users.noreply.github.com>
2021-07-21 10:08:09 +0200

    Modif TestController for new tests

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,20 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Service\FetchData;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/test', name: 'test_')]
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'test')]
-    public function test(FetchData $data): Response
+    #[Route('/api', name: 'api')]
+    public function fetchApi(FetchData $data): Response
     {
         $recipes = $data->fetchRecipes();        
         
         return $this->render('test.html.twig', [
             'recipes' => $recipes
+        ]);
+    }
+
+    #[Route('/form', name: 'form')]
+    public function formIngredients(): Response
+    {
+        $listIngredients = new Ingredient();
+
+        $form = $this->createForm(IngredientType::class, $listIngredients);
+
+        return $this->render('test.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
